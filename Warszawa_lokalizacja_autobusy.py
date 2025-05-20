f7127a2bb5955b60aff8c9832329953043ad22e7
Davvid01 <deberedawid@gmail.com>
2025-05-20 18:35:48 +0200

    poznan

--- Warszawa_lokalizacja_autobusy.py
+++ Warszawa_lokalizacja_autobusy.py
@@ -3,6 +3,20 @@ import pandas as pd
 from sqlalchemy import create_engine, inspect
 import urllib
 
+# Wczytaj klucz API z pliku tekstowego
+def load_api_key(filepath):
+    try:
+        with open(filepath, "r") as file:
+            return file.read().strip()
+    except FileNotFoundError:
+        print(f"Error: File '{filepath}' not found.")
+        exit(1)
+
+# Ścieżka do pliku z kluczem API
+api_key_path = "api_key.txt"
+api_key = load_api_key(api_key_path)
+
+
 conn_str = urllib.parse.quote_plus(
     "Driver={ODBC Driver 18 for SQL Server};"
     "Server=VICTUS\\SQLEXPRESS;"
@@ -16,7 +30,7 @@ inspector = inspect(engine)
 print(inspector.get_table_names())
 
 
-url="https://api.um.warszawa.pl/api/action/busestrams_get/?resource_id=f2e5503e-927d-4ad3-9500-4ab9e55deb59&apikey=ca30886e-6f6f-435a-8b36-4468de811529&type=2"
+url=f"https://api.um.warszawa.pl/api/action/busestrams_get/?resource_id=f2e5503e-927d-4ad3-9500-4ab9e55deb59&apikey={api_key}&type=2"
 response=requests.get(url)
 data=response.json()
 #print(data)
