4a2f0da7cb99c36309d67dcda00007185dc91d85
Davvid01 <deberedawid@gmail.com>
2025-05-30 01:17:45 +0200

    Update Warszawa_API_komunikacja_miejska.py

--- api_clients/Warszawa_API_komunikacja_miejska.py
+++ api_clients/Warszawa_API_komunikacja_miejska.py
@@ -1,6 +1,14 @@
 import requests
 from API_key_warszawa import api_key_warszawa
+import pandas as pd
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
 def fetch_parkingi_parknride():
     url=f"https://api.um.warszawa.pl/api/action/wfsstore_get?id=157648fd-a603-4861-af96-884a8e35b155&apikey={api_key_warszawa}"
-    return api_key_warszawa
+    response = requests.get(url).json()
+    records = response.get("result", {}).get("featureMemberProperties",{})
+    coordinates = response.get("result", {}).get("featureMemberList",{})
+    to_df=pd.DataFrame(records)
+    coord_df=pd.DataFrame(coordinates)
+    return coord_df, to_df
 print(fetch_parkingi_parknride())
