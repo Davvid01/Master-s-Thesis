bd991e9a2b3322a838362964fde9f8763306ec7f
Davvid01 <deberedawid@gmail.com>
2025-05-29 23:18:25 +0200

    Update Wroclaw_CSV.py

--- data_processing/Wroclaw_CSV.py
+++ data_processing/Wroclaw_CSV.py
@@ -10,12 +10,16 @@ zapelnienie_parkingow_48h['Czas_Rejestracji']=pd.to_datetime(zapelnienie_parking
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 
-print(zapelnienie_parkingow_48h.dtypes)
-print("Najstarsza data: ",zapelnienie_parkingow_48h['Czas_Rejestracji'].min(),"Najnowsza data: ", zapelnienie_parkingow_48h['Czas_Rejestracji'].max())
-print("Rekordy z brakiem daty: ",zapelnienie_parkingow_48h[zapelnienie_parkingow_48h['Czas_Rejestracji'].isna()])
-print("Liczba rekordów z brakiem daty: ",zapelnienie_parkingow_48h['Czas_Rejestracji'].isna().sum())
-print("Wiersze z jakimkolwiek brakiem: ",zapelnienie_parkingow_48h[zapelnienie_parkingow_48h.isnull().any(axis=1)])
+def data_check(data):
+    #data['Czas_Rejestracji'] = pd.to_datetime(data['Czas_Rejestracji'], format='mixed', errors='coerce')
+    print(data.dtypes)
+    print("Najstarsza data: ",data['Czas_Rejestracji'].min(),"Najnowsza data: ", data['Czas_Rejestracji'].max())
+    print("Rekordy z brakiem daty: ",data[zapelnienie_parkingow_48h['Czas_Rejestracji'].isna()])
+    print("Liczba rekordów z brakiem daty: ",data['Czas_Rejestracji'].isna().sum())
+    print("Wiersze z jakimkolwiek brakiem: ",data[data.isnull().any(axis=1)])
+    print("W kolejności 'Nazwa',;'Czas_Rejestracji': ", data.sort_values(['Nazwa','Czas_Rejestracji']))
 
 
-print(zapelnienie_parkingow_48h.sort_values(['Nazwa', 'Czas_Rejestracji']))
-#print(zapelnienie_parkingow_48h, zapelnienie_parkingow_biezace)
+print(data_check(zapelnienie_parkingow_48h))
+print(data_check(zapelnienie_parkingow_biezace))
+#print(zapelnienie_parkingow_48h.sort_values(['Nazwa', 'Czas_Rejestracji']))
